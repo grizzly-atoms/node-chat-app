ded0773e7cc3b864fb3fee3e2489bd435607382c
Joshua Kaiser <joshua.b.kaiser@gmail.com>
2019-01-20 09:30:07 -0700

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Ike',
-    text: 'Yo'
-  });
 })
 
 socket.on('disconnect', function () {
